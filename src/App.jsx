e13a180cd1a07a431355db389d810b3565d51640
genialpleasure <isaacnpleasure01@gmail.com>
2025-10-20 20:31:59 +0100

    Investment Calculater

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import Header from "./Components/Header"
+import UserInput from "./Components/UserInput"
+import Resuls from "./Components/Results"
+
 function App() {
+  const[userInput, setUserInput ] = useState({
+        initialInvestment: 0,
+        annualInvestment: 0,
+        expectedReturn: 0,
+        duration: 0
+    });
+
+    const inputIsValid = userInput.duration >= 1; 
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevInput => {
+            return {
+                ...prevInput,
+                [inputIdentifier]: +newValue
+            };
+        });
+    }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+       <Header />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputIsValid && <p className="center">Please enter a valid duration (minimum 1 year).</p>}
+      {inputIsValid && <Resuls userInput={userInput}/>}
+    </>
+
   )
+
 }
 
 export default App
